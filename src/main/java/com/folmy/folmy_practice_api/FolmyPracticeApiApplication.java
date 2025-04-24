9b4d9ef93150cc2fb36cb4c4448b66b41d533528
Maxim Fomov <maksimfomov26@gmail.com>
2025-04-24 20:40:28 +0300

    init: Initialized project

---
+++ src/main/java/com/folmy/folmy_practice_api/FolmyPracticeApiApplication.java
@@ -0,0 +1,13 @@
+package com.folmy.folmy_practice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolmyPracticeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolmyPracticeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folmy/folmy_practice_api/FolmyPracticeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folmy.folmy_practice_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolmyPracticeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
